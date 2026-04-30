1739cce9d2673cc703ffcd21686a9dc01ffcc6e7
Esmeralda Fonseca <esmeralda.fonseca2003@gmail.com>
2026-04-30 23:35:57 +0100

    fix: improve input validation
    - handle extra spaces with strip()
    - normalize case for consistent comparison
    - ensure valid genre selection
    - improve user input validation

--- funcionalidades.py
+++ funcionalidades.py
@@ -1,38 +1,61 @@
 import json
 import ficheiro
 import pandas as pd
-
+from datetime import date
 
 livros=[]
+ano_atual = date.today().year
+genero_literario = ["Romance", "Conto", "Novela", "Fabula", "Didatico"]
+
+#
+def limpar_tela():
+
+    """
+    """
+    import os
+    os.system('clear')
 
 
+#
+def cabecalho(titulo):
+    """
+    """
+    print("*" * 56)
+    print("*", end="")
+    print(f"{titulo:^54}", end="")
+    print("*")
+    print("*" * 56)
+
+#
 def adicionar_livros(livros):
-
     """
     Criar um ficheiro para armazenar o titulo, autor, ano de publicação e genero
     return: um ficheiro com os dados recolhidos
     """
 
-
-    inicio = True
     
-
+#
+    inicio = True
     while inicio:
-        print("1- ADICIONAR LIVRO")
+        limpar_tela()
+        cabecalho("Adicionar livro")
         titulo = input("Titulo do livro: ")
         autor = input("Autor: ")
 
+#
         while True:
-
             ano_de_publicação = input("Ano de publicação: ")
             if ano_de_publicação.isdigit() and len(ano_de_publicação) == 4:
-                ano_de_publicação_int = int(ano_de_publicação)  
-                break              
+                ano_de_publicação_int = int(ano_de_publicação)
+                if ano_de_publicação_int <= ano_atual:
+                    break     
+                else:
+                    print("ERRROR. Data invalidade!")         
                 
             else:
                 print("ERROR. Valor invalido. Tente novamente.")
                 
-
+#
         while True:
 
             estado = input("Já leste esse livro?[S/N] ").upper()
@@ -45,8 +68,17 @@ def adicionar_livros(livros):
             else:
                 print("ERROR. Valor invalido. Tente S para sim e N para não")
             
-
-        genero = input("Genero literario: ")
+#
+        while True:
+            print("Genero literario:")
+            for i in genero_literario:
+                print(f"    -{i}")
+            genero = input("Digite a sua escolha: ").strip().upper()
+            if genero not in genero_literario:
+                print("ERROR. Opção invalida, tente uma das opções disponiveis.")
+            else:
+                genero = genero.title()
+                break
 
         livro ={"Titulo": titulo,
                     "Autor": autor,
@@ -57,6 +89,7 @@ def adicionar_livros(livros):
         livros.append(livro)
         ficheiro.criar_livro(livros)
 
+#
         while True: 
             condição = input("Quer adicionar mais um livro?[S/N]").upper()
            
@@ -68,9 +101,13 @@ def adicionar_livros(livros):
             else:
                 print("ERROR. Valor invalido. Tente S para sim e N para não")
     
-                  
+ #                 
 def limpar_lista(livros):
-    
+    """
+    """
+
+    limpar_tela()
+    cabecalho("Limpar lista")
     condicao = input("Tens a certeza que queres eliminar a tua lista de livros?\n Essa acção é IRRVERSIVEL[S/N]").upper()
     
     if condicao.strip() == "S":
@@ -78,25 +115,42 @@ def limpar_lista(livros):
         with open("livros.json", "w", encoding="utf-8") as ficheiro:
             json.dump([], ficheiro, indent=4)
     
-
+#
 def editar_livro(livros):
-    ...
+    """
+    """
+    limpar_tela()
+    cabecalho("Editar livro")
 
+
+#
 def pesquisar_livro(livros):
+    """
+    """
 
-    if not livros:
-        print("Não existem livros. Adiciona primeiro.")
-        return
-
+    limpar_tela()
+    cabecalho("Pesquisar livro")
     titulo = input(f"Digite o titulo do livro que pretendes encontrar: ").upper()
+    encontrado = False
     for livro in livros:
-        if titulo in livro["Titulo"]:
-            print(f"{livro}: {livros}")
-        else:
-            print(f"ERROR.O livro com o titulo {titulo}, não foi encontrar. Tente novamente.")
+        if titulo in livro["Titulo"].upper():
+            print(f"Livro encontrado: {livro["Titulo"]}")
+            encontrado = True
 
+        if not encontrado:
+            print("Não existem livros. Adiciona primeiro.")
+            return
+
+#       
 def listar_livros(livros):
+    """
+    """
+
+    limpar_tela()
+    cabecalho("Listar livros")
     print(f'Os seus livros sao:')
     tabela = pd.DataFrame(livros)
     return print("\n"* 5, tabela, "\n"*10)
+
+
     
