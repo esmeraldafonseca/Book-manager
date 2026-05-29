e6f54f0e275902264bb2e7be674fd37fafe1471f
Esmeralda Fonseca <esmeralda.fonseca2003@gmail.com>
2026-05-29 23:33:35 +0100

    Refactor: Implementing the match case instruction.
    Use a match case to make the decision easier.

--- funcionalidades.py
+++ funcionalidades.py
@@ -1,6 +1,7 @@
 import json
 import ficheiro
 import pandas as pd
+<<<<<<< HEAD
 from rich import print
 from rich.panel import Panel
 from rich.table import Table
@@ -112,6 +113,43 @@ def adicionar_livros(livros):
 
             estado = input("Já leste esse livro?[S/N] ").upper()
            
+=======
+
+
+livros=[]
+
+
+def adicionar_livros(livros):
+
+    """
+    Criar um ficheiro para armazenar o titulo, autor, ano de publicação e genero
+    return: um ficheiro com os dados recolhidos
+    """
+
+
+    inicio = True
+    
+
+    while inicio:
+        print("1- ADICIONAR LIVRO")
+        titulo = input("Titulo do livro: ")
+        autor = input("Autor: ")
+
+        while True:
+
+            ano_de_publicação = input("Ano de publicação: ")
+            if ano_de_publicação.isdigit() and len(ano_de_publicação) == 4:
+                ano_de_publicação_int = int(ano_de_publicação)  
+                break              
+                
+            else:
+                print("ERROR. Valor invalido. Tente novamente.")
+                
+
+        while True:
+
+            estado = input("Já leste esse livro?[S/N] ").upper()
+>>>>>>> 293704a (feat:project upload)
             if estado == "S":
                 lido = True
                 break
@@ -119,6 +157,7 @@ def adicionar_livros(livros):
                 lido = False
                 break
             else:
+<<<<<<< HEAD
                 erro2()
             
         
@@ -153,6 +192,12 @@ def adicionar_livros(livros):
                     erro2()
                     
                 
+=======
+                print("ERROR. Valor invalido. Tente S para sim e N para não")
+            
+
+        genero = input("Genero literario: ")
+>>>>>>> 293704a (feat:project upload)
 
         livro ={"Titulo": titulo,
                     "Autor": autor,
@@ -163,14 +208,21 @@ def adicionar_livros(livros):
         livros.append(livro)
         ficheiro.criar_livro(livros)
 
+<<<<<<< HEAD
         
         while True: 
             condição = input("Quer adicionar mais um livro?[S/N]").upper()
             
+=======
+        while True: 
+            condição = input("Quer adicionar mais um livro?[S/N]").upper()
+           
+>>>>>>> 293704a (feat:project upload)
             if condição == "S":
                 break
             elif condição == "N":
                 inicio = False
+<<<<<<< HEAD
                 limpar_tela()
                 break
             else:
@@ -352,4 +404,41 @@ def listar_livros(livros):
         
 
 
+=======
+                break
+            else:
+                print("ERROR. Valor invalido. Tente S para sim e N para não")
+    
+                  
+def limpar_lista(livros):
+    
+    condicao = input("Tens a certeza que queres eliminar a tua lista de livros?\n Essa acção é IRRVERSIVEL[S/N]").upper()
+    
+    if condicao.strip() == "S":
+        livros.clear()
+        with open("livros.json", "w", encoding="utf-8") as ficheiro:
+            json.dump([], ficheiro, indent=4)
+    
+
+def editar_livro(livros):
+    ...
+
+def pesquisar_livro(livros):
+
+    if not livros:
+        print("Não existem livros. Adiciona primeiro.")
+        return
+
+    titulo = input(f"Digite o titulo do livro que pretendes encontrar: ").upper()
+    for livro in livros:
+        if titulo in livro["Titulo"]:
+            print(f"{livro}: {livros}")
+        else:
+            print(f"ERROR.O livro com o titulo {titulo}, não foi encontrar. Tente novamente.")
+
+def listar_livros(livros):
+    print(f'Os seus livros sao:')
+    tabela = pd.DataFrame(livros)
+    return print("\n"* 5, tabela, "\n"*10)
+>>>>>>> 293704a (feat:project upload)
     
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """
 Projeto: Sistema de Gestão de Livros
 Autora: Esmeralda Fonseca
@@ -13,10 +14,19 @@ import art #Arte ASCII
 
 inicio = True #flag variable
 OPCOES = ["Adicionar livro", "Listar todos os livros", 
+=======
+import funcionalidades
+import ficheiro
+import art
+
+inicio = True
+opções = ["Adicionar livro", "Listar todos os livros", 
+>>>>>>> 293704a (feat:project upload)
           "Pesquisar livro por titulo", "Editar ", "Apagar lista" , "Sair"]
 livros = ficheiro.carregar_livros()
 
 
+<<<<<<< HEAD
 
 
 print(f'Bem-vindo/a ao seu GESTOR DE LIVROS::',)
@@ -69,5 +79,46 @@ while inicio:
 
 
     
+=======
+print("*" * 56)
+print(f'{"GESTOR DE LIVROS":^56}')
+print("*" * 56)
+print(art.art)
+
+
+while inicio:
+    print(f'Bem-vindo/a ao GESTOR DE LIVROS, escolha uma das opções abaixo.',)
+    for itens, elementos in enumerate(opções, start=1):
+        print(f'{itens}- {elementos}')
+
+    escolha = input("Digite a sua escolha: ").strip()
+
+    if escolha == "":
+        print("ERROR.Campo vazio!")
+    else:
+
+        try:
+            escolha_int = int(escolha)
+        except ValueError:
+            print("ERROR! Valor invalido, por favor digite uma das opções validas(NUMEROS)")
+            continue
+        if escolha_int == 1:
+            funcionalidades.adicionar_livros(livros)
+        elif escolha_int == 2:
+            funcionalidades.listar_livros(livros)
+        elif escolha_int == 3:
+            funcionalidades.pesquisar_livro(livros)
+        elif escolha_int == 4:
+            funcionalidades.editar_livro(livros)
+        elif escolha_int == 5:
+            funcionalidades.limpar_lista(livros)
+        elif escolha_int == 6:
+            print("")
+            print("Obrigado/a pela preferencia, tenha um bom dia e boa leitua.")
+            inicio = False
+        else:
+            print("")
+            print("ERROR. Opção inexistente. Tente novamente ")
+>>>>>>> 293704a (feat:project upload)
       
 
